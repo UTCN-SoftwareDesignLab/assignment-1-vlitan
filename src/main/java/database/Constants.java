7ff5e1132a978dfdc833e959aff71e2a8713a291
virgil <virgilx13@gmail.com>
2018-03-30 11:30:43 +0300

    added role right repository and bootstraping

--- src/main/java/database/Constants.java
+++ src/main/java/database/Constants.java
@@ -8,26 +8,26 @@ import static database.Constants.Roles.*;
 public class Constants {
     public static class Roles {
         public static final String ADMINISTRATOR = "administrator";
-        public static final String EMPLOYEE = "employee";
-        public static final String CUSTOMER = "customer";
+        public static final String USER = "user";
 
-        public static final String[] ROLES = new String[]{ADMINISTRATOR, EMPLOYEE, CUSTOMER};
+        public static final String[] ROLES = new String[]{ADMINISTRATOR, USER};
     }
 
     public static class Rights {
+        public static final String CREATE_CLIENT = "create_client";
+        public static final String DELETE_CLIENT = "delete_client";
+        public static final String UPDATE_CLIENT = "update_client";
+
         public static final String CREATE_USER = "create_user";
         public static final String DELETE_USER = "delete_user";
         public static final String UPDATE_USER = "update_user";
 
-        public static final String CREATE_BOOK = "create_book";
-        public static final String DELETE_BOOK = "delete_book";
-        public static final String UPDATE_BOOK = "update_book";
+        public static final String CREATE_ACCOUNT = "create_account";
+        public static final String DELETE_ACCOUNT = "delete_account";
+        public static final String UPDATE_ACCOUNT = "update_account";
 
-        public static final String SELL_BOOK = "sell_book";
-        public static final String BUY_BOOK = "buy_book";
-        public static final String RETURN_BOOK = "return_book";
 
-        public static final String[] RIGHTS = new String[]{CREATE_USER, DELETE_USER, UPDATE_USER, CREATE_BOOK, DELETE_BOOK, UPDATE_BOOK, SELL_BOOK, BUY_BOOK, RETURN_BOOK};
+        public static final String[] RIGHTS = new String[]{UPDATE_USER, CREATE_USER, DELETE_USER, CREATE_CLIENT, DELETE_CLIENT, UPDATE_CLIENT, CREATE_ACCOUNT, DELETE_ACCOUNT, UPDATE_ACCOUNT};
     }
 
     public static Map<String, List<String>> getRolesRights() {
@@ -37,9 +37,8 @@ public class Constants {
         }
         ROLES_RIGHTS.get(ADMINISTRATOR).addAll(Arrays.asList(RIGHTS));
 
-        ROLES_RIGHTS.get(EMPLOYEE).addAll(Arrays.asList(CREATE_BOOK, DELETE_BOOK, UPDATE_BOOK, SELL_BOOK));
+        ROLES_RIGHTS.get(USER).addAll(Arrays.asList(CREATE_CLIENT, DELETE_CLIENT, UPDATE_CLIENT, CREATE_ACCOUNT, DELETE_ACCOUNT, UPDATE_ACCOUNT));
 
-        ROLES_RIGHTS.get(CUSTOMER).addAll(Arrays.asList(SELL_BOOK, BUY_BOOK, RETURN_BOOK));
 
         return ROLES_RIGHTS;
     }
